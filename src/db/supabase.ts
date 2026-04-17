1a39f59c45766b2d009d8c5461ba2c07ff99277b
tiajnae <ransomtia15@gmail.com>
2026-04-17 13:36:31 -0500

    set up login component

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -1,21 +1,46 @@
-import { createClient } from '@supabase/supabase-js'
-import { Platform } from 'react-native'
+import { createClient, processLock } from '@supabase/supabase-js'
+import { AppState, Platform } from 'react-native'
+import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const supabaseUrl = 'https://api.latechsmp.net'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzc1ODY2NzYzLCJleHAiOjE5MzM1NDY3NjN9.jzCAiod983BBI34dpNoT4CegqRf5AN4kBCPJvNLtOM4'
 
 
-const storage = Platform.OS === 'web' ? undefined : AsyncStorage
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: storage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: Platform.OS === 'web',
-  },
-})
 // To import and use the supabase database system anywhere in the app,
 // add this command to the boilerplate:
 // import { supabase } from '@/db/supabase'
+
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    ...(Platform.OS !== 'web' ? { storage: AsyncStorage } : {}),
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: Platform.OS === 'web',
+    lock: processLock,
+  },
+})
+
+// Tells Supabase Auth to continuously refresh the session automatically
+// if the app is in the foreground. When this is added, you will continue
+// to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
+// `SIGNED_OUT` event if the user's session is terminated. This should
+// only be registered once.
+if (Platform.OS !== 'web') {
+  AppState.addEventListener('change', (state) => {
+    if (state === 'active') {
+      supabase.auth.startAutoRefresh()
+    } else {
+      supabase.auth.stopAutoRefresh()
+    }
+  })
+}
+
+/*
+set up login component
+-creates and exports your Supabase client
+-uses AsyncStorage on mobile to keep the login session saved
+-enables token auto-refresh
+-starts/stops refresh based on whether the app is active on mobile
+*/
